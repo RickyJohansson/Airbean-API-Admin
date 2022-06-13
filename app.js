461125031828aca75461730f0e3defa72ecef910
RickyJohansson <92937315+RickyJohansson@users.noreply.github.com>
2022-06-13 10:58:06 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+app.use(express.json())
+
+const accountRouter = require('./routes/account')
+app.use('/api/account', accountRouter)
+const menuRouter = require('./routes/menu')
+app.use('/api/menu', menuRouter)
+const orderRouter = require('./routes/order')
+app.use('/api/order', orderRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port: ${PORT}`);
+});
+
+
