342cda92cd1e8f2ab611e66925f0458844ea5174
RickyJohansson <Rickjoh91@hotmail.com>
2022-06-15 02:15:03 +0200

    api-keys fixed

--- routes/menu.js
+++ routes/menu.js
@@ -1,37 +1,11 @@
 const { getMenu, addProduct, deleteProduct } = require('../model/menudb')
 const { Router } = require('express');
 
+const { auth } = require('../middleware/auth');
+
 const router = Router();
 
-const apiKeys = [
-    '9234032895hf7347hf444',
-    '7465845hyv23834bt85g',
-    'ah4uh45j4jjjj4444',
-    'lo43k454bfnb43gj4g',
-    '8880888088888088880',
-    '8j7h6g5f4d3s2a1',
-    '0m9n8b7v6c5x4z32'
-];
-
-router.get('/', async (req, res) => {
-    const menuList = await getMenu();
-
-    const resObj = {
-        success: false
-    }
-    if (menuList.length > 0) {
-        resObj.success = true;
-        resObj.order = menuList;
-
-    }
-    else {
-        resObj.message = 'error 420'
-    }
-    res.json(resObj)
-});
-
-
-router.post('/addproduct', async (req, res) => {
+router.post('/addproduct', auth, async (req, res) => {
     const menuList = await addProduct(req.body);
 
     const resObj = {
@@ -42,7 +16,7 @@ router.post('/addproduct', async (req, res) => {
     res.json(resObj);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
     const productId = req.params.id;
     const menuList = await getMenu();
     console.log('menulist:', menuList);
@@ -66,6 +40,23 @@ router.delete('/:id', async (req, res) => {
 
     res.json(resObj);
 
+});
+
+router.get('/', async (req, res) => {
+    const menuList = await getMenu();
+
+    const resObj = {
+        success: false
+    }
+    if (menuList.length > 0) {
+        resObj.success = true;
+        resObj.order = menuList;
+
+    }
+    else {
+        resObj.message = 'error 420'
+    }
+    res.json(resObj)
 });
 
 
